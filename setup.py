9762a10fc9a25cdf4e02275e2ae7095f6ca32c96
Dandan.Xue <tentenxue@gmail.com>
2021-09-28 15:29:42 +0200

    add conda packaging files

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(name='webnma',
       version='3.4',
-      description='Fast normal mode calculation and analyese for protein structures;',
+      description='Fast normal mode calculation and analyese for protein structures',
       url='https://github.com/reuter-group/webnma3',
       author='Dandan Xue, Reuter Lab',
       author_email='dandan.xue@uib.no',
-      license='GPL-3.0 License',
+      license='GPL-3.0',
       package_dir = {'webnma': 'src'},
       packages=['webnma', 'webnma.utils'],
       zip_safe=False,
