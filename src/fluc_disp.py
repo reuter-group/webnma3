Examples with the correct (not squared) normalization

src/fluc_disp.py
@@ -38,7 +38,8 @@ def calc_fluc(modefile:str, mode_nums=[]):
         # use all non-trivial modes 
         es_n = es[6:]
         modes_n = np.array(modes[:,6:]).transpose()
-        
+    
+    # e**2 below to actually have eigenvalues (squares of the frequencies)
     f = lambda coord, e: sum(coord**2)/ e**2
     flucs1 = [[f(a, es_n[i]) for a in m.reshape(ca_num,3)]
                               for i, m in enumerate(modes_n)]
@@ -47,19 +48,18 @@ def calc_fluc(modefile:str, mode_nums=[]):
     s = Units_k_B * TMP / (2. * np.pi) **2 
     flucs = [s * sum(f)/mass[i] for i, f in enumerate(flucs1_tr)]
 
-    # NB! Considering whether the this final transformation is needed in webnma3
-    #return sq_norm(np.array(flucs))
+    normalized_flucs = flucs_norm(np.array(flucs))
     print("\n****************************************************************************")
-    print("NB! calc_fluc() replacing final transformation (squaring and scaling by 100)")
-    print("    by scaling only  [2023 update]")
+    print("NB! calc_fluc() with updated normalization, without squaring [2023 update]")
+    print(f"Sanity check: sum of flucs for all residues is... {sum(normalized_flucs)}")
     print("****************************************************************************\n")
-    return 100 * (np.array(flucs))
-    
+    return normalized_flucs
 
-# squared normalization of a 1D array
-def sq_norm(arr):
-    s = sum(arr**2)
-    return 100 * arr * arr / s
+
+# final transformation above, normalizing values in [0,100]
+def flucs_norm(arr):
+    s = sum(arr)
+    return 100 * arr / s
 
 
 def calc_ss_spans(pdbfile):